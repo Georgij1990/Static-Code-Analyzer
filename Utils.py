5feac9f3713196710aae365ebd683a5503c75a1c
Georgij Kocsarjan <georgij.kocsarjan@gmail.com>
2024-03-19 23:12:58 +0100

    Pass stage 2

--- Utils.py
+++ Utils.py
@@ -1,8 +1,61 @@
+import string
+
+empty_lines = []
+
+
+def add_error(error_codes, line_counter, error_code, error_message):
+    error = f'Line {line_counter}: {error_code} {error_message}'
+    error_codes.append(error)
+
+
 def check_len(line):
     return len(line) > 79
 
 
+def check_indentation(line):
+    counter = 0
+    for i in line:
+        if i.isspace():
+            counter += 1
+        else:
+            break
+    return counter % 4 != 0
+
+
 def check_semicolon(line):
-    if line != "":
+    if line != "" and not line.strip().startswith('#'):
         str_to_check = line.split('#')[0]
         return str_to_check.strip()[-1] == ';'
+
+
+def check_spaces_before_comment(line):
+    strip_line = line.strip()
+    if not strip_line.startswith('#') and '#' in strip_line:
+        str_before_comment = strip_line.split('#')[0]
+        return str_before_comment[-2:] != '  '
+
+
+def find_to_do(line):
+    strip_line = line.strip()
+    if strip_line == '':
+        return False
+    if strip_line.startswith('#'):
+        return 'todo' in strip_line.lower()
+    if '#' in strip_line:
+        return 'todo' in strip_line.split('#')[1].lower()
+
+
+def check_empty_line(line, line_counter):
+    length = len(empty_lines)
+    if line != '' and length != 3:
+        empty_lines.clear()
+        return False
+    if length == 0:
+        empty_lines.append(line_counter)
+        return False
+    if length < 3:
+        empty_lines.append(line_counter)
+        return False
+    if length == 3:
+        empty_lines.clear()
+        return True
--- code_analyzer.py
+++ code_analyzer.py
@@ -1,26 +1,29 @@
-# write your code here C:\Studying\python_scripts\script.txt
+# write your code here C:\Studying\python_scripts\script1.txt
 import Utils
 
 path = input()
-
 errors = []
-
 line_counter = 0
-
-error_codes = {'S001': 'Too long', 'S003': 'Unnecessary semicolon'}
+error_codes = {'S001': 'Too long', 'S002': 'Indentation is not a multiple of four', 'S003': 'Unnecessary semicolon',
+               'S004': 'At least two spaces required before inline comments', 'S005': 'TODO found',
+               'S006': 'More than two blank lines used before this line'}
 error_code = None
 
 with open(path) as file:
-    lines = [line.rstrip() for line in file]
+    lines = [line.rstrip('\n') for line in file]
     for i in lines:
         line_counter += 1
         if Utils.check_len(i):
-            error_code = 'S001'
-            error = f'Line {line_counter}: {error_code} {error_codes[error_code]}'
-            errors.append(error)
+            Utils.add_error(errors, line_counter, 'S001', error_codes['S001'])
+        if Utils.check_indentation(i):
+            Utils.add_error(errors, line_counter, 'S002', error_codes['S002'])
         if Utils.check_semicolon(i):
-            error_code = 'S003'
-            error = f'Line {line_counter}: {error_code} {error_codes[error_code]}'
-            errors.append(error)
+            Utils.add_error(errors, line_counter, 'S003', error_codes['S003'])
+        if Utils.check_spaces_before_comment(i):
+            Utils.add_error(errors, line_counter, 'S004', error_codes['S004'])
+        if Utils.find_to_do(i):
+            Utils.add_error(errors, line_counter, 'S005', error_codes['S005'])
+        if Utils.check_empty_line(i, line_counter):
+            Utils.add_error(errors, line_counter, 'S006', error_codes['S006'])
 
 [print(i) for i in errors]
