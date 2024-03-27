83708d1af4a2d6714e8928a7ee00778eda8ac252
Georgij Kocsarjan <georgij.kocsarjan@gmail.com>
2024-03-27 19:01:45 +0100

    Pass stage 3

--- Utils.py
+++ Utils.py
@@ -1,10 +1,19 @@
+import os.path
 import string
+import argparse
 
 empty_lines = []
 
 
-def add_error(error_codes, line_counter, error_code, error_message):
-    error = f'Line {line_counter}: {error_code} {error_message}'
+def return_argument():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path')
+    arg = parser.parse_args()
+    return arg.path
+
+
+def add_error(error_codes, path_name, line_counter, error_code, error_message):
+    error = f'{path_name}: Line {line_counter}: {error_code} {error_message}'
     error_codes.append(error)
 
 
@@ -59,3 +68,26 @@ def check_empty_line(line, line_counter):
     if length == 3:
         empty_lines.clear()
         return True
+
+
+def check_string(line, path_name, errors, line_counter, error_codes):
+    if check_len(line):
+        add_error(errors, path_name, line_counter, 'S001', error_codes['S001'])
+    if check_indentation(line):
+        add_error(errors, path_name, line_counter, 'S002', error_codes['S002'])
+    if check_semicolon(line):
+        add_error(errors, path_name, line_counter, 'S003', error_codes['S003'])
+    if check_spaces_before_comment(line):
+        add_error(errors, path_name, line_counter, 'S004', error_codes['S004'])
+    if find_to_do(line):
+        add_error(errors, path_name, line_counter, 'S005', error_codes['S005'])
+    if check_empty_line(line, line_counter):
+        add_error(errors, path_name, line_counter, 'S006', error_codes['S006'])
+
+
+def check_file(path, line_counter, errors, error_codes):
+    with open(path) as file:
+        lines = [line.rstrip('\n') for line in file]
+        for i in lines:
+            line_counter += 1
+            check_string(i, path, errors, line_counter, error_codes)
--- code_analyzer.py
+++ code_analyzer.py
@@ -1,7 +1,8 @@
 # write your code here C:\Studying\python_scripts\script1.txt
 import Utils
+import os
 
-path = input()
+path = Utils.return_argument()
 errors = []
 line_counter = 0
 error_codes = {'S001': 'Too long', 'S002': 'Indentation is not a multiple of four', 'S003': 'Unnecessary semicolon',
@@ -9,21 +10,13 @@ error_codes = {'S001': 'Too long', 'S002': 'Indentation is not a multiple of fou
                'S006': 'More than two blank lines used before this line'}
 error_code = None
 
-with open(path) as file:
-    lines = [line.rstrip('\n') for line in file]
-    for i in lines:
-        line_counter += 1
-        if Utils.check_len(i):
-            Utils.add_error(errors, line_counter, 'S001', error_codes['S001'])
-        if Utils.check_indentation(i):
-            Utils.add_error(errors, line_counter, 'S002', error_codes['S002'])
-        if Utils.check_semicolon(i):
-            Utils.add_error(errors, line_counter, 'S003', error_codes['S003'])
-        if Utils.check_spaces_before_comment(i):
-            Utils.add_error(errors, line_counter, 'S004', error_codes['S004'])
-        if Utils.find_to_do(i):
-            Utils.add_error(errors, line_counter, 'S005', error_codes['S005'])
-        if Utils.check_empty_line(i, line_counter):
-            Utils.add_error(errors, line_counter, 'S006', error_codes['S006'])
+isDir = os.path.isdir(path)
+if not isDir:
+    Utils.check_file(path, line_counter, errors, error_codes)
+elif isDir:
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            path_name = os.path.join(root, name)
+            Utils.check_file(path_name, line_counter, errors, error_codes)
 
 [print(i) for i in errors]
